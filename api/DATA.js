58ce816f6acfc62741e38ad56fd16f2102fda720
Nguyễn Hưng <74828782+nguyenhung17072001@users.noreply.github.com>
2022-02-18 14:39:33 +0700

    Delete api directory

--- api/DATA.js
+++
@@ -1,4 +0,0 @@
-const API_KEY = '289734bb17b11a3d48ae4fe8fea91337';
-import React from "react";
-
-const apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=hanoi&appid=289734bb17b11a3d48ae4fe8fea91337';
